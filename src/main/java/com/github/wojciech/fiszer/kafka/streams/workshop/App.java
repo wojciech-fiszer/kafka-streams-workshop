c95b3f8e55d47cb7397914612c98f0c8b06af952
Wojciech Fiszer <fiszer.wojciech@gmail.com>
2019-03-19 22:29:19 +0100

    Added dependencies

---
+++ src/main/java/com/github/wojciech/fiszer/kafka/streams/workshop/App.java
@@ -0,0 +1,8 @@
+package com.github.wojciech.fiszer.kafka.streams.workshop;
+
+public class App {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
